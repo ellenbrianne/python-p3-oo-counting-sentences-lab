a5238252b74746344c68516f8151ba84cccfad62
Ellen Bennett <bennette831@gmail.com>
2023-09-20 23:25:04 -0600

    use string instance methods in class MyString

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self.value = value
+
+    def is_sentence(self):
+        if self.value.endswith(".") == True:
+            return True
+        else: 
+            return False
+    
+    def is_question(self):
+        if self.value.endswith("?") == True:
+            return True
+        else: 
+            return False
+    
+    def is_exclamation(self):
+        if self.value.endswith("!") == True:
+            return True
+        else: 
+            return False
+    
+    def count_sentences(self):
+        pass
+  
+    def get_value(self):
+        return self._value
+  
+    def set_value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else: 
+            print("The value must be a string.")
+
+    value = property(get_value, set_value)
+
+new_string = MyString("This is one. This is two? This is three!")
+new_string.count_sentences()
